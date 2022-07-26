81e233a8d4133607830eb28f48a6991abbf1515c
Farshaid Rafi <farshaid.rafi@gmail.com>
2022-07-25 21:57:41 -0400

    Hello World updated

--- src/Tiger/simple/HelloWorld.java
+++ src/Tiger/simple/HelloWorld.java
@@ -4,6 +4,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");	
-	}
-
-}
+		
+		String abc = "Faroo";
+		String a = " Shhheeeeet";
+		String abc2 = abc.concat(a);
+		
+		System.out.println(abc2);
+		
+		
+	
+	
+	}}
